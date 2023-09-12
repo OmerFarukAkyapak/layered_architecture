f7a877751a312649320822944b8849b411da4383
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-09-12 18:10:34 +0300

    go

---
+++ mse_barncase/Entities/Concrete/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Species { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public int LifeSpan { get; set; }
+        public bool IsAlive { get; set; }
+
+    }
+}
