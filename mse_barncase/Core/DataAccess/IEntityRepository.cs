3a94854ce019373b2e8852ca88b28780f99bad30
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-09-12 19:30:28 +0300

    katmanlı mimari

---
+++ mse_barncase/Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    // generic repository pattern
+    // referans olan ientityden türemiş ve newlenebilen
+    public interface IEntityRepository <T> where T:class,IEntity,new()
+    {
+        T Get(Expression<Func<T, bool>> filter);
+        IList<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
--- mse_barncase/Entities/Concrete/Animal.cs
+++ mse_barncase/Entities/Concrete/Animal.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Animal
+    public class Animal : IEntity
     {
         public int AnimalId { get; set; }
-        public string Species { get; set; }
+        public string AnimalType { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
         public int LifeSpan { get; set; }
         public bool IsAlive { get; set; }
-
+        public int AnimalPrice { get; set; }
     }
 }
