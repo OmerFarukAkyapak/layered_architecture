3aaf10d8f57b167129cc5eac151f352ce8b5a4ea
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-09-12 13:36:31 +0300

    g0

---
+++ mse_barncase/barn_case/Entities/IFarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace barn_case.Class
+{
+    public interface IFarm
+    {
+        void AddAnimal(Animal animal);
+        void AddProduct(Product product);
+        void SellProduct(Product product);
+    }
+}
